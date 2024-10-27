68b7badf669b5bae933e758601db75a255d69c5d
afeef-c <afeefc123@gmail.com>
2024-10-27 14:20:18 +0530

    dockerize backend1.2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ AUTH_USER_MODEL = 'gallery.CustomUser'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
