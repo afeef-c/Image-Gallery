47de326a1661f2cc462f13f969f9ecc1a417759e
afeef-c <afeefc123@gmail.com>
2024-11-29 19:27:32 +0530

    update db

--- frontend/src/components/AuthListener.jsx
+++ frontend/src/components/AuthListener.jsx
@@ -1,16 +1,20 @@
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const AuthListener = () => {
     const authTokens = useSelector((state) => state.auth.authTokens);
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
-        if (!authTokens) {
+        const publicRoutes = ['/login', '/register'];
+
+        // Redirect to login only if user is not authenticated and not on a public route
+        if (!authTokens && !publicRoutes.includes(location.pathname)) {
             navigate('/login');
         }
-    }, [authTokens, navigate]);
+    }, [authTokens, navigate, location.pathname]);
 
     return null;
 };
